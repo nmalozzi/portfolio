4e09cfb53a61c01fb9ba3df2414c8390d8502203
Nick Malozzi <nmalozzi@gmail.com>
2020-01-20 22:07:57 -0700

    updating gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `nickmalozzi.com`,
+    description: `A Portfolio Site for Front End Developer & Designer, Nick Malozzi.`,
+    author: `Nick Malozzi`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
