503f9a2a4dbedd1c5fb719beecb27170401be14a
Nick Malozzi <nmalozzi@gmail.com>
2020-06-09 09:12:01 -0600

    adding some directory structure to components

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -8,7 +8,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-import Header from "./header"
+import Header from "../header/header"
 import style from "./layout.module.scss"
 
 const Layout = ({ children }) => {
