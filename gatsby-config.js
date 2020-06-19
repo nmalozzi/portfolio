1173806995ec528370408a2a73833e213a882596
Nick Malozzi <nmalozzi@gmail.com>
2020-06-19 12:24:37 -0600

    Updating the Favicon and Gatsby Config

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,17 +20,16 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        name: `nickmalozzi.com`,
+        short_name: `nickmalozzi.com`,
+        description: `A Portfolio Site for Front End Developer & Designer, Nick Malozzi.`,
+        lang: `en`,
+        start_url: "/",
+        background_color: "#666666",
+        theme_color: "#666666",
+        display: "standalone",
+        icon: "src/images/favicon.svg",
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
